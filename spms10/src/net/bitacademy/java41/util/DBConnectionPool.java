79fd6b4eaf184f4729271895a989adb064f0ac83
sunjin <sunjin8565@gmail.com>
2013-09-13 09:48:24 +0900

    Update DBConnectionPool.java

--- spms10/src/net/bitacademy/java41/util/DBConnectionPool.java
+++ spms10/src/net/bitacademy/java41/util/DBConnectionPool.java
@@ -3,6 +3,8 @@ package net.bitacademy.java41.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.ArrayList;
+import java.util.Hashtable;
+
 
 public class DBConnectionPool {
 	String dburl;
@@ -11,6 +13,11 @@ public class DBConnectionPool {
 	String driverClass;
 	
 	ArrayList<Connection> list = new ArrayList<Connection>();
+	Hashtable<Thread , Connection > rentTable = new Hashtable<Thread,Connection>();
+	// Map table  허용하지않고 Table null은 널 허용
+	
+	
+	
 	
 	public DBConnectionPool(
 			String dburl, String id, String pwd, String driverClass) {
@@ -21,16 +28,27 @@ public class DBConnectionPool {
 	}
 	
 	public Connection getConnection() throws Exception {
-		if (list.size() > 0) {
-			return list.remove(0);
-		} else {
-			Class.forName(driverClass);
-			return DriverManager.getConnection(dburl, id, pwd);
+		Thread currThread = Thread.currentThread(); 
+
+		Connection con = rentTable.get(currThread);  
+		if(con!= null){
+			return con;
+		}else {
+			if (list.size() > 0) { 
+				con = list.remove(0);
+			} else {  
+				Class.forName(driverClass);
+				con = DriverManager.getConnection(dburl, id, pwd);
+			}
+			rentTable.put(currThread, con); 
+			return con; 
 		}
 	}
 	
 	public void returnConnection(Connection con) {
-		list.add(con);
+		Thread currThread = Thread.currentThread(); 
+		rentTable.remove(currThread); 
+		list.add(con); 
 	}
 }
 
